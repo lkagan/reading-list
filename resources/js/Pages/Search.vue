702401cae5667a3a052cefda1d42bb0d65c2be23
Larry Kagan <larry@superiocity.com>
2021-02-22 06:54:01 -0500

    Change landing page to 'Search'

--- resources/js/Pages/Search.vue
+++ resources/js/Pages/Search.vue
@@ -2,21 +2,12 @@
     <breeze-authenticated-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Search
             </h2>
         </template>
 
         <div class="py-12">
             <search-bar></search-bar>
-        </div>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
         </div>
     </breeze-authenticated-layout>
 </template>
