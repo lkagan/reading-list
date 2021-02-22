0702672af3fbf7257cf2e54b6ef3590a6e3fb5ab
Larry Kagan <larry@superiocity.com>
2021-02-22 02:49:33 -0500

    Update branding

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -2,7 +2,7 @@
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <inertia-link href="/">
-                <breeze-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <breeze-application-logo class="h-20 fill-current text-gray-500" />
             </inertia-link>
         </div>
 
