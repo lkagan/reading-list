aaca0ee07fe97cb6d6d741fad003a57cf9086d85
Larry Kagan <larry@superiocity.com>
2021-02-24 00:46:38 -0500

    Delete book API functionality

--- tests/Feature/BookControllerTest.php
+++ tests/Feature/BookControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Book;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BookControllerTest extends TestCase
@@ -27,5 +28,24 @@ class BookControllerTest extends TestCase
     {
         $data = Book::factory()->create()->only(['title', 'remote_id']);
         $this->post(route('books.store'), $data)->assertRedirect(route('login'));
+    }
+
+    /** @test */
+    public function delete_removes_from_db()
+    {
+        $this->actingAs(User::factory()->create());
+        $book = Book::factory()->create(['user_id' => Auth::id()]);
+        $this->deleteJson(route('books.destroy', $book))->assertSuccessful();
+        $this->assertDatabaseMissing('books', $book);
+    }
+
+    /** @test */
+    public function non_owner_delete_forbidden()
+    {
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+        $book = Book::factory()->create(['user_id' => $user1->id]);
+        $this->actingAs($user2);
+        $this->deleteJson(route('books.destroy', $book))->assertForbidden();
     }
 }
